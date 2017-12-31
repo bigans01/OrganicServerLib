248e4c4820ecce842890a55ea83ed77f7f89f0c4
bigans01 <bigans01@gmail.com>
2017-12-31 17:12:08 -0500

    Add project files.

---
+++ OrganicServerLib/OSServer.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "OSServer.h"
+
+OSServer::OSServer()
+{
+	OrganicSystem Organic(3, 3, 13, 1024, 768);
+}
---
+++ OrganicServerLib/OSServer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include "OrganicSystem.h"
+
+class OSServer
+{
+	OSServer();
+};
